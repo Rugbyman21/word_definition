340cb990a31392d89f6c90151d4adf07c75d7fca
Rugbyman21 <trevorscherling@Trevors-MacBook-Air.local>
2015-09-06 12:32:51 -0700

    passing specs

--- lib/word.rb
+++ lib/word.rb
@@ -14,23 +14,23 @@ class Words
     []
   end
 
-#   define_method(:save) do
-#     @@words.push(self)
-#   end
+  define_method(:save) do
+    @@words.push(self)
+  end
 
   define_singleton_method(:clear) do
     @@words = []
   end
 
-#   define_singleton_method(:find) do |id|
-#     found_words = nil
-#     @@words.each() do |words|
-#       if words.id().eql?(id)
-#         found_words = words
-#       end
-#     end
-#
-#   found_words
-#
-#   end
+  define_singleton_method(:find) do |id|
+    found_words = nil
+    @@words.each() do |words|
+      if words.id().eql?(id)
+        found_words = words
+      end
+    end
+
+  found_words
+
+  end
  end
